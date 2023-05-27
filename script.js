2c2f27b06c0f8761f21320ff35f42b55ca27a587
rockybul <rockyhasan403@gmail.com>
2023-05-27 09:31:26 +0530

    compeleted project

--- script.js
+++ script.js
@@ -1,4 +1,7 @@
-const futureDate = new Date(2023, 4, 30, 5, 10).getTime();
+
+function display() {
+
+const futureDate = new Date(2023, 10, 27, 9, 45).getTime();
 const currentDate = new Date().getTime();
 const t = futureDate - currentDate
 const oneDay = 24 * 60 * 60 * 1000;
@@ -6,7 +9,14 @@ const oneHour = 60 * 60 * 1000;
 const oneMinute = 60 * 1000;
 const oneSecond = 1000;
 
+let days = Math.floor(t / oneDay)
 let hours =  Math.floor((t % oneDay) / oneHour);
+let minutes = Math.floor((t % oneHour) / oneMinute)
+let seconds = Math.floor((t % oneMinute) / 1000); 
+ 
+
+
+
 // console.log(hours);
 
 
@@ -17,5 +27,32 @@ let tempDate = new Date();
 let tempYear = tempDate.getFullYear();
 let tempMonth = tempDate.getMonth();
 let tempDay = tempDate.getDate()
+
+let value = [days, hours, minutes, seconds];
+
+function checkFunc(element) {
+    if(element < 10) {
+        return `0${element}`
+    }else {
+        return element
+    }
+}
+
+const display = document.querySelectorAll('.flex h2');
+
+display.forEach((elm, index)=> {
+    elm.textContent = checkFunc(value[index]);
+})
+
+if(t < 0) {
+    display.forEach(elm => {
+        elm.textContent = `00`;
+    })
+    
+}
+}
+
+setInterval(display, 1000)
+
 
 
